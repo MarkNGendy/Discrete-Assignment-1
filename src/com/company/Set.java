7d59ef8da06b0a73e85df0fa5b171c2e45c3f0bb
Mark <mark74384@gmail.com>
2020-10-27 21:34:10 +0200

    Update Set.java
    Union of two sets

--- src/com/company/Set.java
+++ src/com/company/Set.java
@@ -19,5 +19,19 @@ public class Set {
         }
         return false;
     }
+    static void Union(Set s1 , Set s2){
+        Set Union = new Set(s1.size + s2.size);
+        Union.setSize(0);
+        for (int i=0 ; i<s1.size ; i++){
+            Union.elements[i] = s1.elements[i];
+            Union.setSize(Union.size+1);
+        }
+        for (int i=0 ; i<s2.size ; i++){
+            if (!Union.contains(s2.elements[i])){
+                Union.elements[Union.size] = s2.elements[i];
+                Union.setSize(Union.size+1);
+            }
+        }
+    }
 
 }
